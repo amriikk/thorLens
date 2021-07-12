bab386b69397b782c62f6faf7b4a7d115d8cc645
John Trujillo <amriikk@gmail.com>
2021-07-12 03:45:17 -0700

    add eye left point

--- test-01.js
+++ test-01.js
@@ -1,6 +1,8 @@
 let video;
 let noseX = 0;
 let noseY = 0;
+let eyeLX = 0;
+let eyeLY = 0;
 
 function setup() {
     createCanvas(640,480);
@@ -13,10 +15,14 @@ function setup() {
 function gotPoses(poses) {
     console.log(poses);
     if (poses.length > 0) {
-        let newX = poses[0].pose.keypoints[0].position.x;
-        let newY = poses[0].pose.keypoints[0].position.y;
-        noseX =  lerp(noseX, newX, 0.5);
-        noseY =  lerp(noseY, newY, 0.5);
+        let nX = poses[0].pose.keypoints[0].position.x;
+        let nY = poses[0].pose.keypoints[0].position.y;
+        let eX = poses[0].pose.keypoints[1].position.x;
+        let eY = poses[0].pose.keypoints[1].position.y;
+        noseX =  lerp(noseX, nX, 0.5);
+        noseY =  lerp(noseY, nY, 0.5);
+        noseX =  lerp(noseX, eX, 0.5);
+        noseY =  lerp(noseY, eY, 0.5);
     }
 }
 
