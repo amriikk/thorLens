7e2bc323065f5d3c0a5a79fcd0cc8d9904ae5b10
John Trujillo <amriikk@gmail.com>
2021-07-12 02:09:07 -0700

    get noseX + noseY

--- test-01.js
+++ test-01.js
@@ -1,14 +1,19 @@
 let video;
+let noseX = 0;
+let noseY = 0;
 
 function setup() {
     createCanvas(640,480);
     video = createCapture(VIDEO);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
 
 function gotPoses(poses) {
     console.log(poses);
+    noseX = poses[0].pose.keypoints[0].position.x;
+    noseY = poses[0].pose.keypoints[0].position.y;
 }
 
 function modelLoaded() {
@@ -18,4 +23,9 @@ function modelLoaded() {
 function draw() {
     image(video, 0, 0)
     filter(GRAY);
+
+    fill(255,0,0);
+    ellipse(noseX, noseY, 80);
+
+
 }
